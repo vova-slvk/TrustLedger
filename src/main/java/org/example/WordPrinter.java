a115be6f7ec0897d4714e36382a7a76a2464e240
vl7dmqk <v.solotvinskij@gmail.com>
2024-10-03 18:00:59 +0300

    Dodano funkcj wypisywania wyrazu od tyu

--- src/main/java/org/example/WordPrinter.java
+++ src/main/java/org/example/WordPrinter.java
@@ -1,18 +1,11 @@
-package org.example;
-
 import java.util.Scanner;
 
 public class WordPrinter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Podaj jakiś wyraz: ");
-        String input = scanner.nextLine();
-
-        // Wypisanie wyrazu od tyłu
-        String reversed = new StringBuilder(input).reverse().toString();
-        System.out.println("Wprowadzony wyraz od tyłu to: " + reversed);
-
-        scanner.close();
+        System.out.print("Podaj wyraz: ");
+        String word = scanner.nextLine();
+        String reversedWord = new StringBuilder(word).reverse().toString();
+        System.out.println("Wyraz od tyłu: " + reversedWord);
     }
 }
