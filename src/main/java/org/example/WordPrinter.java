b49bfcdf31cc55b606d31fd5a18d407d8a570015
vl7dmqk <v.solotvinskij@gmail.com>
2024-10-03 18:21:25 +0300

    Zmiana sposobu przyjmowania danych na sta warto

--- src/main/java/org/example/WordPrinter.java
+++ src/main/java/org/example/WordPrinter.java
@@ -1,10 +1,8 @@
-import java.util.Scanner;
+package org.example;
 
 public class WordPrinter {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Podaj wyraz: ");
-        String word = scanner.nextLine();
+        String word = "world"; // zmieniamy na stałą wartość
         String reversedWord = new StringBuilder(word).reverse().toString();
         System.out.println("Wyraz od tyłu: " + reversedWord);
     }
